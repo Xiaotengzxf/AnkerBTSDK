55ee034549c09a20043675213e89b6fcb4849504
bruce.zhang <315082431@qq.com>
2018-06-14 18:46:22 +0800

    modify spec

---
+++ AnkerBTSDK/Classes/BT/ClassicManager.swift
@@ -0,0 +1,12 @@
+//
+//  ClassicManager.swift
+//  AnkerBTSDK
+//
+//  Created by ANKER on 2018/5/4.
+//
+
+import UIKit
+
+class ClassicManager: NSObject {
+
+}
---
+++ AnkerBTSDK/Classes/BT/ClassicManagerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ClassicManagerDelegate.swift
+//  AnkerBTSDK
+//
+//  Created by ANKER on 2018/5/5.
+//
+
+import UIKit
+
+class ClassicManagerDelegate: NSObject {
+
+}
